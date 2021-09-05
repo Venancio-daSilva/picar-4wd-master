d5d3a005e99ef40f51cb23d9cb39ac28d835fe48
Venancio-daSilva <90118470+Venancio-daSilva@users.noreply.github.com>
2021-09-04 20:52:19 -0400

    Initial commit

---
+++ picar-4wd-master/picar_4wd/lab1_part2.py
@@ -0,0 +1,27 @@
+import picar_4wd as fc
+import time
+import numpy
+
+def main():
+    my_map
+    #Step 1 make a map
+    generate_map()
+    #Step 2 create a route, add offset for size of car
+
+    #Step 3 OpenCV print what the camera sees
+
+    #Step 4 move to target on map
+
+    #Repeat
+
+def generate_map():
+
+
+
+if __name__ == "__main__":
+    try:
+        while True:
+            #loop for ever, or till something crashes
+            main()
+    finally: 
+        fc.stop()
